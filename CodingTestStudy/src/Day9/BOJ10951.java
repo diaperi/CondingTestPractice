626d8162120540e7d4d00fa5a6c7cc19b0603b28
diaperi0901@gmail.com <diaperi0901@gmail.com>
2024-06-24 21:05:51 +0900

    Day09

---
+++ CodingTestStudy/src/Day9/BOJ10951.java
@@ -0,0 +1,14 @@
+package Day9;
+
+import java.util.Scanner;
+
+public class BOJ10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){   // 다음에 받을게 있는지 확인
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+    }
+}
