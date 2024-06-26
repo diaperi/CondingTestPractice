eced6787c0fc9d798579102276111f2d65f7d348
diaperi0901@gmail.com <diaperi0901@gmail.com>
2024-06-27 00:10:36 +0900

    Day12

---
+++ CodingTestStudy/src/Day12/BOJ11718.java
@@ -0,0 +1,13 @@
+package Day12;
+
+import java.util.Scanner;
+
+public class BOJ11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){ // 다음것이 있는지 판단 후 실행
+            String text = sc.nextLine(); // 한줄을 통으로 받음
+            System.out.println(text);
+        }
+    }
+}
